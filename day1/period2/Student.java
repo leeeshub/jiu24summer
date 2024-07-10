a4e210c78fc47ff90850e6d4e7d601bc9ec329bc
Hyoin Lee <hiliee09@naver.com>
2024-07-10 02:59:14 -0700

    Add fields and methods for Student class

--- day1/period2/Student.java
+++ day1/period2/Student.java
@@ -1,5 +1,44 @@
 package jiu24summer.day1.period2;
 
 public class Student {
-    
+    private String studentId;
+    private int grade;
+    private String major;
+
+    // constructor
+    public Student(String name, int age, String gender, String studentId) {
+        super(name, age, gender);
+        this.studentId = studentId;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Student ID: " + studentId);
+    }
+
+    // getter and setter
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String newStudentId) {
+        studentId = newStudentId;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int newGrade) {
+        grade = newGrade;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String newMajor) {
+        major = newMajor;
+    }
 }
