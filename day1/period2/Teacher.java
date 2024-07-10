aaee0041a0dcab672787b7b5dbdb679f7187cb40
Hyoin Lee <hiliee09@naver.com>
2024-07-10 03:06:58 -0700

    Add fields and methods for Teacher class

--- day1/period2/Teacher.java
+++ day1/period2/Teacher.java
@@ -1,5 +1,25 @@
 package jiu24summer.day1.period2;
 
-public class Teacher {
-    
+public class Teacher extends Person {
+    private String subject;
+
+    public Teacher(String name, int age, String gender, String subject) {
+        super(name, age, gender);
+        this.subject = subject;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Subject: " + subject);
+    }
+
+    // getter and setter
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String newSubject) {
+        subject = newSubject;
+    }
 }
