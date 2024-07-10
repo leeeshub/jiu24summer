3544302043f302db81f629e38149a6717d74f91e
Hyoin Lee <hiliee09@naver.com>
2024-07-10 02:54:00 -0700

    Add fields and methods for Person class

--- day1/period2/Person.java
+++ day1/period2/Person.java
@@ -1,5 +1,44 @@
 package jiu24summer.day1.period2;
 
 public class Person {
+    private String name;
+    private int age;
+    private String gender;      // M or F
+
+    // constructor
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    // method
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", Age: " + age + ", Gender: " + gender);
+    }
+
+    // getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+        age = newAge;
+    }
+
+    public String getGender(String newGender) {
+        return gender;
+    }
     
+    public void setGender(String newGender) {
+        gender = newGender;
+    }
 }
