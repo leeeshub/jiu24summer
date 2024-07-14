28c6ad46b75939ef67ff6c603ceb9bd1cca4199b
Hyoin Lee <hiliee09@naver.com>
2024-07-14 17:01:22 +0900

    Create classes extend Teacher

---
+++ src/day1/period2/EnglishTeacher.java
@@ -0,0 +1,12 @@
+public class EnglishTeacher extends Teacher {
+
+    public EnglishTeacher(String name, int age, String gender) {
+        super(name, age, gender, "English");
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("This teacher specializes in English.");
+    }
+}
---
+++ src/day1/period2/MathTeacher.java
@@ -0,0 +1,12 @@
+public class MathTeacher extends Teacher {
+
+    public MathTeacher(String name, int age, String gender) {
+        super(name, age, gender, "Math");
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("This teacher specializes in Mathematics.");
+    }
+}
